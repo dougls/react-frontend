953336f50d07e468520c9b7cda132eb67f4bc594
Douglas Martins <douglas@github.com>
2025-03-22 13:50:44 -0300

    update development

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Application in development.
         </p>
         <a
           className="App-link"
