044b24ec4f2e5e0236413ac38412aa1bca73ed27
Douglas Martins <douglas@github.com>
2025-03-22 15:43:32 -0300

    feat: test rule environment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Application in development.
+          Application in production.
         </p>
         <a
           className="App-link"
